7d04beffa241c929e230c9c34ed07f935cd81fb2
Niranjan <niranjan0881@gmail.com>
2022-12-05 12:54:51 +0530

    latest

--- app.js
+++ app.js
@@ -8,20 +8,51 @@
 //A wrapper to manage the code that is running
 // let myVar = 1;
 
-function b() {
-  var myVar;
-  console.log(myVar);
-}
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
 
-function a() {
-  var myVar = 2;
-  console.log(myVar);
-  b();
-}
+// function a() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   b();
+// }
 
-var myVar = 1;
-console.log(myVar);
-a();
-console.log(myVar);
+// var myVar = 1;
+// console.log(myVar);
+// a();
+// console.log(myVar);
 
 //Output  1 2 undefined 1
+
+//Lexical Environment
+
+// function a() {
+//   function b() {
+//     console.log(myVar);
+//   }
+//   var myVar = 1;
+//   b();
+// }
+
+// a();
+
+//Asynchronous Callbacks
+// function waitThreeSeconds() {
+//   //   setTimeout(() => {
+//   //     console.log("finished function");
+//   //   }, 3000);
+//   var ms = 3000 + new Date().getTime();
+//   while (new Date() < ms) {}
+//   console.log("finished function");
+// }
+
+// function clickHandler() {
+//   console.log("Clicked");
+// }
+
+// document.addEventListener("click", clickHandler);
+
+// waitThreeSeconds();
+// console.log("Finished Execution");
